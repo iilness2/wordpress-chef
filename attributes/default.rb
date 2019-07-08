07fd14f056e69bcfbb798d1bb4177cb8491beb39
iilness2 <andre.aliaman90@gmail.com>
2019-07-08 23:09:03 +0700

    update

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
 # site name
-default['wordpress']['server_name'] ='pusatatapmodern.printaja.ga'
-# root directory, it will be /var/www/pusatatapmodern.printaja.ga
+default['wordpress']['server_name'] ='/*DOMAIN_NAME*/'
+# root directory, it will be /var/www/'/*DOMAIN_NAME*/
 default['wordpress']['document_root'] = "/var/www/#{node['wordpress']['server_name']}"
 # default config file
 default['wordpress']['default_conf'] = '/etc/httpd/conf/httpd.conf'
@@ -8,15 +8,15 @@ default['wordpress']['default_conf'] = '/etc/httpd/conf/httpd.conf'
 default['wordpress']['server_admin'] ='andre.aliaman90@gmail.com'
 # log dir for the site
 default['wordpress']['log_dir'] = '/var/log/www'
-# Error log for the site, it will be /var/log/pusatatapmodern.printaja.ga-error.log
+# Error log for the site, it will be /var/log/'/*DOMAIN_NAME*/-error.log
 default['wordpress']['error_log'] = "#{node['wordpress']['log_dir']}/""#{node['wordpress']['server_name']}-error.log"
-# Access log for the site, it will be /var/log/pusatatapmodern.printaja.ga-access.log
+# Access log for the site, it will be /var/log/'/*DOMAIN_NAME*/-access.log
 default['wordpress']['custom_log'] = "#{node['wordpress']['log_dir']}/""#{node['wordpress']['server_name']}-access.log"
 # The root password for MySQL
-default['wordpress']['mysql_password'] = 'YoaQWE.!@#1123'
+default['wordpress']['mysql_password'] = ''/*YOUR_PASSWORD*/'
 # The name of the WordPress database
-default['wordpress']['wordpress_database'] = 'apaini'
+default['wordpress']['wordpress_database'] = '/*YOUR_DATABASE*/'
 # The default username for the WordPress database
-default['wordpress']['wordpress_username'] = 'queue'
+default['wordpress']['wordpress_username'] = '/*DATABASE_USERNAME*/'
 # The default password for the WordPress database
-default['wordpress']['wordpress_password'] = 'W0rdYoaQWE.!@#1123Pr3$$PWD'
+default['wordpress']['wordpress_password'] = '/*WORDPRESSS+PASSWORD*/'
